8029a5822bb702a387673f25c2c766cbccf256ef
Ashitosh Ghate <61339195+ashitoshghate@users.noreply.github.com>
2021-11-03 20:39:24 +0530

    Mysql Object Oriented in php
    This file is for object oriented connection here I also mention the error which come with version change

---
+++ mysqlobjectoriented.php
@@ -0,0 +1,24 @@
+//Mysql object oriented 
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+/////Note on the object-oriented example above:
+
+//$connect_error was broken until PHP 5.2.9 and 5.3.0. If you need to ensure compatibility with PHP versions prior to 5.2.9 and 5.3.0, use the following code instead:
+
+// Check connection
+//if (mysqli_connect_error()) {
+ // die("Database connection failed: " . mysqli_connect_error());
+//}
